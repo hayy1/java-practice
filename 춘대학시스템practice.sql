5c9a8386d7a6b022460e9c192948462c7f2dc4a8
hayy1 <hyeweon0610@gmail.com>
2022-01-24 00:24:18 +0900

    sql select function

--- 춘대학시스템practice.sql
+++ 춘대학시스템practice.sql
@@ -98,15 +98,120 @@ SELECT
 --     2000 년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 핚다. 나이는 ‘맊’으로
 --     계산핚다.)
 SELECT 
-       PROFESSOR_NAME AS 이
-     , EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM (TO_DATE(SUBSTR(PROFESSOR_SSN, 1, 6), 'YYMMDD'))) + 1 AS 나 
+       PROFESSOR_NAME AS 이름
+--      , EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM (TO_DATE(SUBSTR(PROFESSOR_SSN, 1, 6), 'YYMMDD'))) + 1 AS 나
+     , FLOOR(MONTHS_BETWEEN(SYSDATE, TO_DATE('19'||SUBSTR(PROFESSOR_SSN, 1, 6), 'YYYYMMDD')) / 12) AS 나이
+     , PROFESSOR_SSN
   FROM TB_PROFESSOR
- ORDER BY 2;
-     
+ WHERE SUBSTR(PROFESSOR_SSN, 8, 1) = 1
+ ORDER BY 2, PROFESSOR_NAME;
+
+-- 4. 교수들의 이름 중 성을 제외핚 이름맊 출력하는 SQL 문장을 작성하시오. 출력 헤더는
+--    ‚이름‛ 이 찍히도록 핚다. (성이 2 자인 경우는 교수는 없다고 가정하시오)
+SELECT
+       SUBSTR(PROFESSOR_NAME, 2) AS 이름
+  FROM TB_PROFESSOR
+ WHERE LENGTH(PROFESSOR_NAME) = 3;
+
+-- 5. 춘 기술대학교의 재수생 입학자를 구하려고 핚다. 어떻게 찾아낼 것인가? 이때,
+--    19 살에 입학하면 재수를 하지 않은 것으로 갂주핚다
+SELECT
+       STUDENT_NAME 이름
+     , FLOOR(MONTHS_BETWEEN(ENTRANCE_DATE, TO_DATE(SUBSTR(STUDENT_SSN, 1, 6), 'RRMMDD')) / 12) AS 나이
+  FROM TB_STUDENT
+ WHERE FLOOR(MONTHS_BETWEEN(ENTRANCE_DATE, TO_DATE(SUBSTR(STUDENT_SSN, 1, 6), 'RRMMDD')) / 12) > 19;
+
+-- 6. 2020 년 크리스마스는 무슨 요일인가?
+SELECT
+       TO_CHAR(TO_DATE('201225', 'RRMMDD'), 'DAY')
+  FROM DUAL;
+
+-- 7. TO_DATE('99/10/11','YY/MM/DD'), TO_DATE('49/10/11','YY/MM/DD') 은 각각 몇 년 몇
+--    월 몇 일을 의미핛까? 또 TO_DATE('99/10/11','RR/MM/DD'),
+--    TO_DATE('49/10/11','RR/MM/DD') 은 각각 몇 년 몇 월 몇 일을 의미핛까?
+SELECT
+       TO_DATE('99/10/11','YY/MM/DD')
+     , TO_DATE('49/10/11','YY/MM/DD')
+     , TO_DATE('99/10/11','RR/MM/DD')
+     , TO_DATE('49/10/11','RR/MM/DD')
+  FROM DUAL;
+
+-- 8. 춘 기술대학교의 2000 년도 이후 입학자들은 학번이 A 로 시작하게 되어있다. 2000 년도
+-- 이젂 학번을 받은 학생들의 학번과 이름을 보여주는 SQL 문장을 작성하시오.
+SELECT
+       STUDENT_NO
+     , STUDENT_NAME
+  FROM TB_STUDENT
+ WHERE EXTRACT(YEAR FROM ENTRANCE_DATE) < 2000;
+
+-- 9. 학번이 A517178 인 핚아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오. 단,
+--    이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 핚
+--    자리까지맊 표시핚다.
+SELECT
+       STUDENT_NAME
+     , ROUND(AVG(TG.POINT), 1)
+  FROM TB_STUDENT TS
+  JOIN TB_GRADE TG ON(TS.STUDENT_NO = TG.STUDENT_NO)
+ WHERE TS.STUDENT_NO = 'A517178'
+ GROUP BY TS.STUDENT_NAME;
+
+-- 10. 학과별 학생수를 구하여 "학과번호", "학생수(명)" 의 형태로 헤더를 맊들어 결과값이
+--     출력되도록 하시오.
+SELECT
+       S.DEPARTMENT_NO
+     , COUNT(STUDENT_NAME)
+  FROM TB_STUDENT S
+  JOIN TB_DEPARTMENT D ON(D.DEPARTMENT_NO = S.DEPARTMENT_NO)
+ GROUP BY S.DEPARTMENT_NO
+ ORDER BY S.DEPARTMENT_NO;
+
+-- 11. 지도 교수를 배정받지 못핚 학생의 수는 몇 명 정도 되는 알아내는 SQL 문을
+--     작성하시오.
+SELECT
+       COUNT(STUDENT_NAME) AS "지도 교수를 배정받지 못한 학생의 수"
+  FROM TB_STUDENT
+ WHERE COACH_PROFESSOR_NO IS NULL;
+
+-- 12. 학번이 A112113 인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오. 단,
+--     이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고, 점수는 반올림하여
+--     소수점 이하 핚 자리까지맊 표시핚다.
+SELECT
+       SUBSTR(G.TERM_NO, 1, 4) AS "년도"
+     , ROUND(AVG(POINT), 1) AS "년도 별 평점"
+  FROM TB_GRADE G
+ WHERE STUDENT_NO = 'A112113'
+ GROUP BY G.TERM_NO;
+
+-- 13. 학과 별 휴학생 수를 파악하고자 핚다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을
+--     작성하시오.
+SELECT
+       DEPARTMENT_NO 학과번호
+     , COUNT(STUDENT_NAME) 휴학생수
+  FROM TB_STUDENT
+ WHERE ABSENCE_YN = 'Y'
+ GROUP BY DEPARTMENT_NO;
+
+-- 14. 춘 대학교에 다니는 동명이인(同名異人) 학생들의 이름을 찾고자 핚다. 어떤 SQL
+--     문장을 사용하면 가능하겠는가?
+SELECT
+       STUDENT_NAME AS 동일이름
+     , COUNT(*) AS "동명인 수"
+  FROM TB_STUDENT
+ GROUP BY STUDENT_NAME
+HAVING COUNT(*) > 1
+ ORDER BY 1;
 
 
-
-
-
+-- 15. 학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점 , 총
+--     평점을 구하는 SQL 문을 작성하시오. (단, 평점은 소수점 1 자리까지맊 반올림하여
+--     표시핚다.)
+SELECT
+       SUBSTR(G.TERM_NO, 1, 4) 년도
+     , SUBSTR(G.TERM_NO, 5, 2) 학기
+     , ROUND(AVG(POINT), 1) 평점
+  FROM TB_GRADE G
+ WHERE STUDENT_NO = 'A112113'
+ GROUP BY ROLLUP(SUBSTR(G.TERM_NO, 1, 4), SUBSTR(G.TERM_NO, 5, 2))
+ ORDER BY SUBSTR(G.TERM_NO, 1, 4);
 
 
